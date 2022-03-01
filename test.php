9dbe1ed3cb9e3cadb05a06ee78036edbc286b61a
unknown <morolamisi35@gmail.com>
2022-03-01 08:20:11 +0000

    first commit

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+
+//printing or echo to declare
+
+//creating variables
+use the dollar sign to start a variables
+$variableName = value;
+strings are put in a single coat or double coat
+$userName = 'morolam@ucc.edu.gh'//string
+echo $userName
+
+$num = 12.67; //double
+echo 'GHS' . $num
+
+to concartinate,use [.]
+
+$fname = '<br>Lamisi';
+$lname = 'Queen';
+
+echo $fname . ' ' . $lname; //concat
+?>
